8b5b593490e60b1f89ca53e4b2c61e6ea0b7dae3
Ivan Bondaruk <bonda.ivan22@gmail.com>
2020-10-15 22:28:09 +0300

    1

--- LaboratorniRaboti/Labaratorna_2_1/Source.cpp
+++ LaboratorniRaboti/Labaratorna_2_1/Source.cpp
@@ -1,9 +1,28 @@
+// Дано три числа. Знайти найменше з них.
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	system("pause")
-		return 0;
+    int a, b, c = 0;
+
+    cout << "Enter a, b, c: ";
+    cin >> a, b, c;
+
+    if (a > b)
+    {
+        if (b > c)
+            cout << c;
+        else
+            cout << b;
+    }
+    else
+    {
+        if (a < b && a < c)
+            cout << a;
+    }
+	system("pause");
+	return 0;
 }
